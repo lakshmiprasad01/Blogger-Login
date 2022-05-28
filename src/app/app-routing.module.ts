8bdc295ebe0883dcd401af6353914249174ceefb
lakshmiprasad <prasad.malakala@gmail.com>
2022-05-28 09:15:41 +0530

    loginform added to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder ,FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
+  isSubmitted  =  false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fB: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.loginForm  =  this.fB.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+  get formControls() { return this.loginForm.controls; }
+
+  login(){
+    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if(this.loginForm.invalid){
+      return;
+    }
+    this.authService.login(this.loginForm.value);
+    this.router.navigateByUrl('/admin');
+  }
+
+}
